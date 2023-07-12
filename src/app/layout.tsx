45b49355f74723cad7d755ae7d67a5081b6b70d9
SlimCandy <91788772+slimcandy@users.noreply.github.com>
2023-07-12 20:15:39 +0200

    Add font swap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,11 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Noto_Serif, Playfair_Display } from 'next/font/google'
 
-export const noto = Noto_Serif({ subsets: ['latin'] })
-export const playfair = Playfair_Display({ subsets: ['latin'] })
+export const noto = Noto_Serif({ subsets: ['latin'], display: 'swap' })
+export const playfair = Playfair_Display({
+  subsets: ['latin'],
+  display: 'swap'
+})
 
 export const metadata: Metadata = {
   title: 'RSS Reader',
